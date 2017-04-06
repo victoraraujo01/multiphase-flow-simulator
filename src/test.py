294b70152846ceb188aae1533b6a3b044a85aa05
Victor Moreira Araújo <victoraraujo01@gmail.com>
2017-04-05 22:26:34 -0300

    Changed iteration logic: now we increment the pressure at variable rates This method is significantly faster than the previous method, where we iterated over the depth of the tubing advancing by a fixed delta_l

--- src/test.py
+++ src/test.py
@@ -8,35 +8,54 @@ from flow import Flow
 from tubing import Tubing
 import helpers
 
-well_head_pressure = 5.0
-temperature = 175.0
-delta_l = 1
+well_head_pressure = 150.0
+temperature = 170.0
 
 x = []
 y = []
 
-for flow_rate in np.arange(1, 2000, 100):
-    tubing = Tubing(8000, 1.995, 90.0, 0.000902255639097744)
-    gas = Gas(0.65)
+for flow_rate in np.arange(1, 1000, 10):
+    print(flow_rate)
+    tubing = Tubing(10000, 1.995, 90.0, 0.00015)
+    gas = Gas(0.7)
     oil = Oil(25.0, gas)
     water = Water(1.07, gas)
-    flow = Flow(flow_rate, 10.0, 0, tubing)
+    flow = Flow(flow_rate, 600.0, 0, tubing)
 
     bubble_point = helpers.bubble_point(temperature, oil, water, flow)
+
+    last_pressure = 0.0
     pressure = well_head_pressure
-    for l in np.arange(0, tubing.length, delta_l):
+    last_depth = 0.0
+    depth = 0.0
+
+    delta_p = 0.5
+    last_limit = 5.0
+    limit = 10.0
+    while depth < tubing.length:
         gas.update_conditions(pressure, temperature)
         oil.update_conditions(pressure, bubble_point, temperature, flow.water_cut)
         water.update_conditions(pressure, bubble_point, temperature, flow.water_cut)
         flow.update_conditions(pressure, bubble_point, gas, oil, water)
 
-        delta_p = flow.grav_pressure_gradient + flow.fric_pressure_gradient
-        pressure = pressure - delta_p
+        dp_dz = flow.grav_pressure_gradient + flow.fric_pressure_gradient
+        last_depth = depth
+        depth = depth + delta_p / abs(dp_dz)
+
+        last_pressure = pressure
+        pressure = pressure + delta_p
+
+        if pressure >= limit:
+            delta_p = limit / 10.0
+            tmp_limit = limit
+            limit = last_limit * 10.0
+            last_limit = tmp_limit
+
+    factor = (tubing.length - last_depth) / (depth - last_depth)
+    pressure = last_pressure + factor * (pressure - last_pressure)
 
     x.append(flow_rate)
     y.append(pressure)
-    print(flow_rate)
-    print(pressure)
 
 plt.plot(x, y)
 plt.grid(True)
