2b48fcc4723ad3366948df9a60363d725b57edb7
Victor Moreira Araújo <victoraraujo01@gmail.com>
2017-04-09 22:22:18 -0300

    Updated IPR class to be created based on production test data

--- src/ipr.py
+++ src/ipr.py
@@ -1,21 +1,50 @@
+import src.ipr_tests_analysis as analysis
+
 class IPR(object):
 
-    def __init__(self, avg_pressure, bubble_point, undersaturated_ip):
+    def __init__(self, b_param, avg_pressure, bubble_point, undersaturated_ip):
         self.avg_pressure = avg_pressure
         self.bubble_point = bubble_point
         self.undersaturated_ip = undersaturated_ip
-        self.b_param = -0.2
+        self.b_param = b_param
 
         self.flow_rate_at_bubble_point = self.calc_flow_rate_at_bubble_point()
         self.max_flow_rate = self.calc_max_flow_rate()
+
+    @classmethod
+    def from_tests(cls, b_param, bubble_point, first_test, secnd_test):
+        high_pressure_test = first_test if first_test[0] > secnd_test[0] else secnd_test
+        low_pressure_test = first_test if first_test[0] <= secnd_test[0] else secnd_test
+
+        high_pressure, _ = high_pressure_test
+        low_pressure, _ = low_pressure_test
+
+        result = None
+        if high_pressure > bubble_point and low_pressure > bubble_point:
+            result = analysis.tests_above_bp(high_pressure_test, low_pressure_test)
+        elif high_pressure > bubble_point and low_pressure < bubble_point:
+            result = analysis.tests_on_opposite_sides_of_bp(
+                b_param, bubble_point, high_pressure_test, low_pressure_test
+            )
+        else:
+            result = analysis.tests_below_bp(
+                b_param, bubble_point, high_pressure_test, low_pressure_test
+            )
+
+        avg_pressure, undersaturated_ip = result
+        return cls(b_param, avg_pressure, bubble_point, undersaturated_ip)
 
     def calc_flow_rate_at_bubble_point(self):
         return max(0.0, (self.avg_pressure - self.bubble_point) * self.undersaturated_ip)
 
     def calc_max_flow_rate(self):
+        pressure = self.bubble_point
+        if self.avg_pressure < self.bubble_point:
+            pressure = self.avg_pressure
+
         qmax = (
             self.flow_rate_at_bubble_point +
-            self.undersaturated_ip * self.bubble_point /
+            self.undersaturated_ip * pressure /
             (2.0 + self.b_param)
         )
         return qmax
